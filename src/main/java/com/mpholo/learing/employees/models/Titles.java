bae7f7dc48484fc0202a9a8ed1eb7941f48b8a10
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-14 10:17:23 +0200

    Converted titles entity to JPA Entity. Closes #5

--- src/main/java/com/mpholo/learing/employees/models/Titles.java
+++ src/main/java/com/mpholo/learing/employees/models/Titles.java
@@ -5,18 +5,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Titles extends  BaseEntity {
 
     private String title;
     private LocalDate fromDate;
     private LocalDate toDate;
 
+    @ManyToOne
+    @JoinColumn(name="employees_id")
     private Employees employees;
     private Period period;
 
