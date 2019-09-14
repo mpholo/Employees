f8f5411794b50535096a6766a0582580f27e8fd7
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-14 10:12:09 +0200

    Converted employees entity to JPA Entity. Closes #6

--- src/main/java/com/mpholo/learing/employees/models/Employees.java
+++ src/main/java/com/mpholo/learing/employees/models/Employees.java
@@ -6,12 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.List;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Employees extends BaseEntity {
 
     private LocalDate birthDate;
@@ -19,6 +24,8 @@ public class Employees extends BaseEntity {
     private String lastName;
     Gender gender;
     private Local hireDate;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "employees")
+    private List<Salaries> salariesList;
 
 
 
