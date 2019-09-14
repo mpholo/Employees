979b96b26c52aa9f9142b71622184ca2d1e27dcb
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-14 09:30:54 +0200

    Added BaseEnity. Closes #9

---
+++ src/main/java/com/mpholo/learing/employees/models/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.mpholo.learing.employees.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public abstract class BaseEntity {
+
+    private Long id;
+
+}
