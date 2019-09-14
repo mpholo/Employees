8305c201f9c5810421002e745a465d6edba844e4
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-14 09:45:11 +0200

    added salaries Entity. Closes #4

---
+++ src/main/java/com/mpholo/learing/employees/models/Salaries.java
@@ -0,0 +1,26 @@
+package com.mpholo.learing.employees.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salaries extends BaseEntity {
+
+    private int salary;
+    private Set<Employees> employees = new HashSet<>();
+    private LocalDate fromDate;
+    private LocalDate toDate;
+
+
+}
