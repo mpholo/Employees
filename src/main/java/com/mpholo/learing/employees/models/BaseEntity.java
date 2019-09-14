b0d0ce7ac475537887dfc5b1d0e6f5c39cfda5c4
Mpholo Leboea <mpholo.leboea@vcontractor.co.za>
2019-09-14 10:02:15 +0200

    Converted BaseEntity to JPA Entity. Closes #11

--- src/main/java/com/mpholo/learing/employees/models/BaseEntity.java
+++ src/main/java/com/mpholo/learing/employees/models/BaseEntity.java
@@ -3,10 +3,17 @@ package com.mpholo.learing.employees.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
+@Entity
+@MappedSuperclass
 public abstract class BaseEntity {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
