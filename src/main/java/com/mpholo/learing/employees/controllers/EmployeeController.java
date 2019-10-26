ed67ce6757ad502b625f0ef32b2b18736c6d7a7b
Mpholo Leboea <mpholo.leboea@gmail.com>
2019-10-26 07:03:14 +0200

    added handler to display all employees. Closes #21

---
+++ src/main/java/com/mpholo/learing/employees/controllers/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.mpholo.learing.employees.controllers;
+
+import com.mpholo.learing.employees.services.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String showAll(Model model) {
+        return "/employees/showall";
+    }
+}
